97b270f9ea201509703c07a6548acd402d96f3a6
crevillo <crevillo@gmail.com>
2011-03-18 21:36:40 +0100

    added custom notification event class. it extends eznotificationevent, only for setting two more fields for the event (status and message) when creating notification events see doc inside for more info

---
+++ classes/eznotificationeventanta.php
@@ -0,0 +1,25 @@
+<?php 
+
+class eZNotificationEventTanta extends eZNotificationEvent
+{
+    static function create( $type, $params = array() )
+    {
+        $row = array(
+            "id" => null,
+            'event_type_string' => $type,
+            'data_int1' => 0,
+            'data_int2' => isset( $params['messagetype'] ) ?  $params['messagetype'] : 0,
+            'data_int3' => 0,
+            'data_int4' => 0,
+            'data_text1' => '',
+            'data_text2' => isset( $params['messagetext'] ) ?  $params['messagetext'] : '',
+            'data_text3' => '',
+            'data_text4' => '' );
+        $event = new eZNotificationEvent( $row );
+        eZDebugSetting::writeDebug( 'kernel-notification', $event, "event" );
+        $event->initializeEventType( $params );
+        return $event;
+    }
+}
+
+?>
