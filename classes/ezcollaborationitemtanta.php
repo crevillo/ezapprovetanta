f497d1b82ddf43f2461f2cbf83b71af98a75ebf6
crevillo <crevillo@gmail.com>
2011-03-18 21:35:12 +0100

    added custom collaboration item class
    will allow to create kinda personalized notification events.

---
+++ classes/ezcollaborationitemtanta.php
@@ -0,0 +1,19 @@
+<?php 
+class eZCollaborationItemTanta
+{
+    function createNotificationEvent( $collaborationItem, $subType = false, $messagetype = 0, $messagetext = '' )
+    {
+        $handler = $collaborationItem->attribute( 'handler' );
+        $info = $handler->attribute( 'info' );
+        $type = $info['type-identifier'];
+        if ( $subType )
+            $type .= '_' . $subType;
+        $event = eZNotificationEventTanta::create( 'ezcollaboration', array( 'collaboration_id' => $collaborationItem->attribute( 'id' ),
+                                                                        'collaboration_identifier' => $type,
+                                                                        'messagetype' => $messagetype,
+                                                                        'messagetext' => $messagetext ) );
+        $event->store();
+        return $event;
+    }
+}
+?>
