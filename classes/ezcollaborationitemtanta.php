1f81380120a510f64e451a535ebff9f2620d4ee3
crevillo <crevillo@gmail.com>
2011-03-22 10:39:45 +0100

    distinguish our items from the default ones (marking thema s ezcollaborationtanta)

--- classes/ezcollaborationitemtanta.php
+++ classes/ezcollaborationitemtanta.php
@@ -8,7 +8,7 @@ class eZCollaborationItemTanta
         $type = $info['type-identifier'];
         if ( $subType )
             $type .= '_' . $subType;
-        $event = eZNotificationEventTanta::create( 'ezcollaboration', array( 'collaboration_id' => $collaborationItem->attribute( 'id' ),
+        $event = eZNotificationEventTanta::create( 'ezcollaborationtanta', array( 'collaboration_id' => $collaborationItem->attribute( 'id' ),
                                                                              'collaboration_identifier' => $type,
                                                                              'messagetype' => $messagetype,
                                                                              'messagetext' => $messagetext ) );
