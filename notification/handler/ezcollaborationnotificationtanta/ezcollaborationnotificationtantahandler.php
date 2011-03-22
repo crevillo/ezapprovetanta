884eae7d325b0fe6905c663de3c7881ce681f025
crevillo <crevillo@gmail.com>
2011-03-22 16:38:36 +0100

    fixed bad class name

---
+++ notification/handler/ezcollaborationnotificationtanta/ezcollaborationnotificationtantahandler.php
@@ -0,0 +1,46 @@
+<?php
+
+class eZCollaborationNotificationTantaHandler extends eZCollaborationNotificationHandler
+{
+    const NOTIFICATION_HANDLER_ID = 'ezcollaborationtanta';
+    const TRANSPORT = 'ezmail';
+
+    /*!
+     Constructor
+    */
+    function eZCollaborationNotificationTantaHandler()
+    {
+        $this->eZNotificationEventHandler( self::NOTIFICATION_HANDLER_ID, "Tanta Collaboration Handler" );
+    }    
+
+    function handle( $event )
+    {
+        die( 'xxx' );
+        eZDebugSetting::writeDebug( 'kernel-notification', $event, "trying to handle event" );
+        if ( $event->attribute( 'event_type_string' ) == self::NOTIFICATION_HANDLER_ID )
+        {
+            $parameters = array();
+            $status = $this->handleCollaborationEvent( $event, $parameters );
+          
+            if ( $status == eZNotificationEventHandler::EVENT_HANDLED )
+                $this->sendMessage( $event, $parameters );
+            else
+                return false;
+        }
+        return true;
+    }
+
+    function handleCollaborationEvent( $event, &$parameters )
+    {
+        die( 'this way' );
+        $collaborationItem = $event->attribute( 'content' );
+        if ( !$collaborationItem )
+            return eZNotificationEventHandler::EVENT_SKIPPED;
+        $collaborationHandler = $collaborationItem->attribute( 'handler' );
+        die( $collaborationHandler );
+        return $collaborationHandler->handleCollaborationEvent( $event, $collaborationItem, $parameters );
+    }   
+}
+
+?>
+
