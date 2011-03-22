b798acf07969ae2e93fcae43087ce0dc3cd3e039
crevillo <crevillo@gmail.com>
2011-03-22 11:17:28 +0100

    telling ez our extension will contain custom event and notification handler

---
+++ settings/notification.ini.append.php
@@ -0,0 +1,12 @@
+<?php /*
+[NotificationEventTypeSettings]
+RepositoryDirectories[]=extension/ezapprovetanta/notification/event/
+ExtensionDirectories[]=ezapprovetanta
+AvailableNotificationEventTypes[]=ezcollaborationtanta
+
+[NotificationEventHandlerSettings]
+RepositoryDirectories[]=extension/ezapprovetanta/notification/handler/
+ExtensionDirectories[]=ezapprovetanta
+AvailableNotificationEventTypes[]=ezcollaborationnotificationtanta
+
+*/ ?>
