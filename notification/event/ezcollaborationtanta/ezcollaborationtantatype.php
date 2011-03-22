082b4fe3960d2c626b9cad4515493c421b8de6f9
crevillo <crevillo@gmail.com>
2011-03-22 20:25:23 +0100

    added some doc changed constructor definiton to php5 style

--- notification/event/ezcollaborationtanta/ezcollaborationtantatype.php
+++ notification/event/ezcollaborationtanta/ezcollaborationtantatype.php
@@ -1,16 +1,22 @@
 <?php
+/**
+ * File containing the eZCollaborationEventTantaType class. 
+ *
+ * @copyright <CopyrightString>
+ * @license <LicenseString>
+ * @version <VersionString>
+ * @package <Package>
+ * @subpackage <SubPackage>
+ */
 
 class eZCollaborationEventTantaType extends eZCollaborationEventType
 {
     const NOTIFICATION_TYPE_STRING = 'ezcollaborationtanta';
 
-    /*!
-     Constructor
-    */
-    function eZCollaborationEventTantaType()
+    function __construct()
     {
         $this->eZNotificationEventType( self::NOTIFICATION_TYPE_STRING );
-    }    
+    }
 }
 
 eZNotificationEventType::register( eZCollaborationEventTantaType::NOTIFICATION_TYPE_STRING, 'eZCollaborationEventTantaType' );
